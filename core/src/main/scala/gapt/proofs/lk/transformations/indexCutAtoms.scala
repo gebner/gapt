be96467641866f200951e4600e3072fdb91f8d9f
Gabriel Ebner <gebner@gebner.org>
2020-10-05 17:44:34 +0200

    Add function to add indices to atoms in cut formulas.

---
+++ core/src/main/scala/gapt/proofs/lk/transformations/indexCutAtoms.scala
@@ -0,0 +1,123 @@
+package gapt.proofs.lk.transformations
+
+import gapt.expr.Const
+import gapt.proofs.{ Ant, HOLSequent, Suc }
+import gapt.proofs.context.mutable.MutableContext
+import gapt.proofs.lk._
+import gapt.proofs.lk.rules._
+import gapt.expr.formula._
+import gapt.expr.formula.hol.instantiate
+import gapt.utils.Maybe
+
+object indexCutAtoms {
+
+  def apply( p: LKProof )( implicit ctx: Maybe[MutableContext] ): LKProof =
+    apply( p, p.endSequent )( ctx.getOrElse( MutableContext.guess( p ) ) )
+
+  def rename( f: Formula )( implicit ctx: MutableContext ): Formula = f match {
+    case f @ Atom( c: Const, args ) =>
+      Atom( ctx.addDefinition( c, ctx.newNameGenerator.freshWithIndex( c.name ), reuse = false ), args )
+    case Iff( a, b )      => Iff( rename( a ), rename( b ) )
+    case And( a, b )      => And( rename( a ), rename( b ) )
+    case Or( a, b )       => Or( rename( a ), rename( b ) )
+    case Imp( a, b )      => Imp( rename( a ), rename( b ) )
+    case Neg( a )         => Neg( rename( a ) )
+    case All( x, a )      => All( x, rename( a ) )
+    case Ex( x, a )       => Ex( x, rename( a ) )
+    case Top() | Bottom() => f
+    case Eq( _, _ )       => f
+  }
+
+  def apply( p: LKProof, es: HOLSequent )( implicit ctx: MutableContext ): LKProof = p match {
+
+    case p @ CutRule( q, i, r, j ) =>
+      val newCutF = rename( p.cutFormula )
+      CutRule( apply( q, p.getLeftSequentConnector.parent( es, newCutF ) ), i,
+        apply( r, p.getRightSequentConnector.parent( es, newCutF ) ), j )
+
+    case LogicalAxiom( a ) =>
+      if ( es( Ant( 0 ) ) == es( Suc( 0 ) ) ) LogicalAxiom( es( Ant( 0 ) ) )
+      else ProofLink( Bottom(), es )
+    case TopAxiom | BottomAxiom => p
+    case ReflexivityAxiom( _ )  => p
+
+    case p @ WeakeningLeftRule( q, f ) =>
+      WeakeningLeftRule( apply( q, p.getSequentConnector.parent( es ) ), f )
+    case p @ WeakeningRightRule( q, f ) =>
+      WeakeningRightRule( apply( q, p.getSequentConnector.parent( es ) ), f )
+
+    case p @ ContractionLeftRule( q, i, j ) =>
+      ContractionLeftRule( apply( q, p.getSequentConnector.parent( es ) ), i, j )
+    case p @ ContractionRightRule( q, i, j ) =>
+      ContractionRightRule( apply( q, p.getSequentConnector.parent( es ) ), i, j )
+
+    case p @ NegLeftRule( q, i ) =>
+      val Neg( f ) = es( p.mainIndices.head )
+      NegLeftRule( apply( q, p.getSequentConnector.parent( es ).updated( i, f ) ), i )
+    case p @ NegRightRule( q, i ) =>
+      val Neg( f ) = es( p.mainIndices.head )
+      NegRightRule( apply( q, p.getSequentConnector.parent( es ).updated( i, f ) ), i )
+
+    case p @ AndLeftRule( q, i, j ) =>
+      val And( f, g ) = es( p.mainIndices.head )
+      AndLeftRule( apply( q, p.getSequentConnector.parent( es ).updated( i, f ).updated( j, g ) ), i, j )
+    case p @ AndRightRule( q, i, r, j ) =>
+      val And( f, g ) = es( p.mainIndices.head )
+      AndRightRule(
+        apply( q, p.getLeftSequentConnector.parent( es ).updated( i, f ) ), i,
+        apply( r, p.getRightSequentConnector.parent( es ).updated( j, g ) ), j )
+
+    case p @ OrRightRule( q, i, j ) =>
+      val Or( f, g ) = es( p.mainIndices.head )
+      OrRightRule( apply( q, p.getSequentConnector.parent( es ).updated( i, f ).updated( j, g ) ), i, j )
+    case p @ OrLeftRule( q, i, r, j ) =>
+      val Or( f, g ) = es( p.mainIndices.head )
+      OrLeftRule(
+        apply( q, p.getLeftSequentConnector.parent( es ).updated( i, f ) ), i,
+        apply( r, p.getRightSequentConnector.parent( es ).updated( j, g ) ), j )
+
+    case p @ ImpRightRule( q, i, j ) =>
+      val Imp( f, g ) = es( p.mainIndices.head )
+      ImpRightRule( apply( q, p.getSequentConnector.parent( es ).updated( i, f ).updated( j, g ) ), i, j )
+    case p @ ImpLeftRule( q, i, r, j ) =>
+      val Imp( f, g ) = es( p.mainIndices.head )
+      ImpLeftRule(
+        apply( q, p.getLeftSequentConnector.parent( es ).updated( i, f ) ), i,
+        apply( r, p.getRightSequentConnector.parent( es ).updated( j, g ) ), j )
+
+    case p @ ForallLeftRule( q, i, _, t, _ ) =>
+      val newMain = es( p.mainIndices.head )
+      ForallLeftRule(
+        apply( q, p.getSequentConnector.parent( es ).updated( i, instantiate( newMain, t ) ) ),
+        i, newMain, t )
+    case p @ ExistsRightRule( q, i, _, t, _ ) =>
+      val newMain = es( p.mainIndices.head )
+      ExistsRightRule(
+        apply( q, p.getSequentConnector.parent( es ).updated( i, instantiate( newMain, t ) ) ),
+        i, newMain, t )
+
+    case p @ ForallRightRule( q, i, ev, _ ) =>
+      val newMain = es( p.mainIndices.head )
+      ForallRightRule(
+        apply( q, p.getSequentConnector.parent( es ).updated( i, instantiate( newMain, ev ) ) ),
+        i, newMain, ev )
+    case p @ ExistsLeftRule( q, i, ev, _ ) =>
+      val newMain = es( p.mainIndices.head )
+      ExistsLeftRule(
+        apply( q, p.getSequentConnector.parent( es ).updated( i, instantiate( newMain, ev ) ) ),
+        i, newMain, ev )
+
+    case p @ ForallSkRightRule( q, i, _, skT ) =>
+      val newMain = es( p.mainIndices.head )
+      ForallSkRightRule(
+        apply( q, p.getSequentConnector.parent( es ).updated( i, instantiate( newMain, skT ) ) ),
+        i, newMain, skT )
+    case p @ ExistsSkLeftRule( q, i, _, skT ) =>
+      val newMain = es( p.mainIndices.head )
+      ExistsSkLeftRule(
+        apply( q, p.getSequentConnector.parent( es ).updated( i, instantiate( newMain, skT ) ) ),
+        i, newMain, skT )
+
+  }
+
+}
